e457dc06aa0e9eccd41f790e608fbc0b1e8584af
KristenHam <kristenfernandez7@gmail.com>
2015-03-01 13:43:25 -0500

    pulled turtle state out of l_system

---
+++ TurtleState.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ * Class for holding turtle drawer state for use with stack
+ * Composed of an angle and a position, getters and setters for both
+ */
+public class TurtleState {
+	private int angle;
+	private Vector3 position;
+	
+	public TurtleState(int angle, Vector3 pos) {
+		this.angle = angle;
+		this.position = pos;
+	}
+	
+	public void setAngle(int angle) {
+		this.angle = angle;
+	}
+	public void setPosition (Vector3 pos) {
+		this.position = pos;
+	}
+	public int getAngle() {
+		return angle;
+	}
+	public Vector3 getPosition() {
+		return position;
+	}
+}
--- l_system.cs
+++ l_system.cs
@@ -4,33 +4,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 
 /*
- * Class for holding turtle drawer state for use with stack
- * Composed of an angle and a position, getters and setters for both
- */
-public class TurtleState {
-	private int angle;
-	private Vector3 position;
-
-	public TurtleState(int angle, Vector3 pos) {
-		this.angle = angle;
-		this.position = pos;
-	}
-
-	public void setAngle(int angle) {
-		this.angle = angle;
-	}
-	public void setPosition (Vector3 pos) {
-		this.position = pos;
-	}
-	public int getAngle() {
-		return angle;
-	}
-	public Vector3 getPosition() {
-		return position;
-	}
-}
-
-/*
  * The main L system creator class
  */
 public class l_system : MonoBehaviour {
